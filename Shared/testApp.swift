fae3504cb8474622aeaff499e50aed767148013c
kunsang-philips <kunsang.waygal@philips.com>
2021-06-20 00:49:02 +0530

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Kunsang Wangyal on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
