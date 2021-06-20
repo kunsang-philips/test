25180f1acb7e4bbbf0aa7b4ca73afe81e1d56fec
kunsang-philips <kunsang.waygal@philips.com>
2021-06-20 14:03:39 +0530

    initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerCard="card2"
+    @State private var cpuCard="card3"
+    @State private var playerScore=0
+    @State private var cpuScore=0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Spacer()
+            Image("ironman")
+            Spacer()
+            HStack{
+                Spacer()
+                Image(playerCard)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 150)
+                Spacer()
+                Image(cpuCard)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 150)
+                Spacer()
+            }
+            Button(action: {
+                let playerRand=Int.random(in:2...14)
+                let cpuRand=Int.random(in:2...14)
+                playerCard="card"+String(playerRand)
+                cpuCard="card"+String(cpuRand)
+                if(playerRand>cpuRand){
+                    playerScore+=1
+                }else if(cpuRand>playerRand){
+                    cpuScore+=1
+                }
+                
+            }, label: {
+                Image("fight")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)            })
+            
+            Spacer()
+            HStack{
+                Spacer()
+                
+                VStack(spacing: 10.0){
+                    Text("Player1")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                    Text(String(playerScore))
+                        .font(.title)
+                        .foregroundColor(Color.red)
+                }
+                Spacer()
+                
+                VStack{
+                    Text("CPU")
+                        .font(.title)
+                        .foregroundColor(Color.blue)
+                    Text(String(cpuScore))
+                        .font(.title)
+                        .foregroundColor(Color.red)
+                }
+                Spacer()
+                
+            }
+            Spacer()
+            
+        }
     }
 }
 
